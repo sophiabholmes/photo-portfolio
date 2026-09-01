c421c8218c959f49e9cea57a8626646f8f926f8f
sophiabholmes <123424773+sophiabholmes@users.noreply.github.com>
2026-09-01 18:57:31 +0100

    Add new images to photos object
    Updated the photos object to include new images across various categories including home, bts, events, portraits, and objects.

--- photos.js
+++ photos.js
@@ -1,21 +1,59 @@
 const photos = {
   home: [
+    { src: "images/home/58120028.JPG", alt: "", caption: "" },
   ],
 
   bts: [
-    {
-      src: "1.png",
-      alt: "Behind the scenes",
-      caption: "elephantine, 2024"
-    },
+    { src: "images/bts/1.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/2.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/3.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/4.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/5.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/6.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/7.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/8.png", alt: "", caption: "elephantine" },
+    { src: "images/bts/9.jpg", alt: "", caption: "habitat hotel" },
+    { src: "images/bts/10.png", alt: "", caption: "habitat hotel" },
+    { src: "images/bts/11.png", alt: "", caption: "habitat hotel" },
+    { src: "images/bts/12.png", alt: "", caption: "habitat hotel" },
   ],
 
   events: [
+    { src: "images/events/EVENTS-01.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-02.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-03.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-04.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-05.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-06.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-07.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-08.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-09.jpg", alt: "", caption: "" },
+    { src: "images/events/EVENTS-10.jpg", alt: "", caption: "" }
   ],
 
   portraits: [
+    { src: "images/portraits/PORTRAITS-01.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-02.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-03.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-04.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-05.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-06.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-07.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-08.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-09.jpg", alt: "", caption: "" },
+    { src: "images/portraits/PORTRAITS-10.jpg", alt: "", caption: "" }
   ],
 
   objects: [
+    { src: "images/objects/OBJECTS-01.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-02.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-03.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-04.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-05.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-06.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-07.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-08.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-09.jpg", alt: "", caption: "" },
+    { src: "images/objects/OBJECTS-10.jpg", alt: "", caption: "" }
   ]
 };
